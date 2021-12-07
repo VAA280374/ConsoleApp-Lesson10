0f99d4b9815e8c42d93a9e7ff525219e3272da1c
User <User@ARCHITECT-PC>
2021-12-07 22:57:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppуLesson10/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppуLesson10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle = new Angle();
+            double andleRad = 0;
+            int temp;
+
+            Console.WriteLine("Введите значение угла раздельно градусы-минутыц-секунды");
+            Console.WriteLine();
+
+            Console.Write(" Введите целое число градусов, знак определяет направление вращения : ");
+            temp = Convert.ToInt32(Console.ReadLine());
+            angle.Gradus = temp;
+           
+            Console.Write(" Введите целое неотрицательное число угловых минут, менее 60 : ");
+            temp = Convert.ToInt32(Console.ReadLine());
+            angle.Minets = temp;
+            
+            Console.Write(" Введите целое неотрицательное число угловых секунд, менее 60 : ");
+            temp = Convert.ToInt32(Console.ReadLine());
+            angle.Seconds = temp;
+            Console.WriteLine();
+
+            andleRad = angle.ToRadians();
+            Console.WriteLine(" Введенный угол, в радианах, составляет : {0}", andleRad);
+
+            Console.ReadKey();
+        }
+    }
+    class Angle
+    {
+        int pointer = 1; // Задает направление вращения (в соответствии с тригонометрическим кругом) : + против часовойй стрелки, - по часовой стрелки.
+        int gradus; // целое число градусов, знак определяет направление вращения (в соответствии с тригонометрическим кругом).
+        int minets; // целое число угловых минут В ДИАППАЗОНЕ [0;60) , отложенное по направлению, заданному знаком gradus, 
+        int seconds; // целое число угловых секунд В ДИАППАЗОНЕ [0;60) , отложенное по направлению, заданному знаком gradus, 
+
+        public int Gradus
+        {
+            set // "Препарируем" значение для раздельного хранения.
+            { 
+                int g = value;
+                if (g < 0)
+                { 
+                    this.pointer = -1;
+                    g = g * (-1);
+                }
+                this.gradus = g; 
+            }
+            get // Возвращаем пользовательское значение.
+            {
+                int g = this.gradus * this.pointer;
+                return g; 
+            } 
+        }
+        public int Minets
+        {
+            set
+            {
+                if ((value >= 0) & (value < 60))
+                {
+                    this.minets = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значени минут должно быть менее 60 и положительным.");
+                }
+
+            }
+            get
+            { return this.minets; }
+        }
+        public int Seconds
+        {
+            set
+            {
+                if ((value >= 0) & (value < 60))
+                {
+                    this.seconds = value;
+                }
+                else
+                {
+                    Console.WriteLine("Значени секунд должно быть менее 60 и положительным.");
+                }
+
+            }
+            get
+            { return this.minets; }
+        }
+
+        public double ToRadians()
+        {
+            double r;
+            r = this.pointer * ( this.gradus + 60 * ( this.minets + 60 * this.seconds ) );
+            r = Math.PI *  r / 180;
+            return r;
+
+        }
+
+
+    }
+}
---
+++ ConsoleAppуLesson10/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleAppуLesson10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppуLesson10")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("09e77f55-0c25-43ca-8a35-f1c5b582353b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
